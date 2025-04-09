571e6cfdcfde13b95fc267a163cfce43fb825a1d
angresgarroalumnosupm.es <andresgarro@alumnos.upm.es.com>
2025-04-09 20:32:31 +0200

    socket added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 using System;
 using System.Threading;
+using System.Net;
+using System.Net.Sockets; //Import Sockets
+using System.Text;
 using EasyModbus; // Imports EasyModbusCore
 
 class Program
@@ -17,18 +19,60 @@ class Program
         ModbusClient modbusClient = new ModbusClient(ipAddress, port);
         try
         {
-            modbusClient.Connect(); // Connecting to Modbus Server
+            // Connecting to Modbus Server
+            modbusClient.Connect(); 
             Console.WriteLine($"Connecting a {ipAddress}:{port}");
+
+
+            // Container Name of the LISTENER Point
+            string nameContainerListener = "RpiTemperatureSensorModule"; 
+            
+            // Dns.GetHostAddresses gets IP address of the container in the Docker network.
+            //Socket is EXPOSED in port 8888
+            IPEndPoint ListernerPoint1 = new IPEndPoint(Dns.GetHostAddresses(nameContainerListener)[0], 8888);
+            //
+
+            // Create a SOCKET TCP/IP to SEND Plc DATA
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // Connect to the LISTENER POINT
+                sender.Connect(ListernerPoint1);    
+                Console.WriteLine("Connected a {0}", sender.RemoteEndPoint);
+                
                 while(true)
                 {
                     int startAddress = 0;  // Address of the Input register which will be read
                     int[] registers = modbusClient.ReadInputRegisters(startAddress, 1); // Reading 1 register
 
+                    // Convert the integer to array of bytes
+                    byte[] bytes = BitConverter.GetBytes(registers[0]);
+                
+                    // Send the data through the Socket
+                    int bytesSent = sender.Send(bytes);
+
+                    Console.WriteLine("SERVER SOCKET of the PLC Container SENDING!");
                     Console.WriteLine($"Date Time: {DateTime.Now} - Value of Register {startAddress}: {registers[0]}");
-                    Console.WriteLine($"Time sleep 3s. Tres segundos");
-                    // wait for 3 seconds before the next reading 
+                    //Console.WriteLine("Data sent: {0}", registers[0]); 
+                    
+                    // Wait for 3 seconds before the next reading 
                     Thread.Sleep(3000);
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            //If there is a error in the loop and the loop ends, the connection is closed
+            finally
+            {
+                if (sender.Connected)
+                {
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                    Console.WriteLine("Connection closed for PLC Container.");
+                }
+            }
         }
         catch (Exception ex)
         {
