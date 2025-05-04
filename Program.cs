6136482c7887a04327d66c52d234eb6b1a215899
angresgarroalumnosupm.es <andresgarro@alumnos.upm.es.com>
2025-05-04 17:21:36 +0200

    mqtt subcription to newscale topic

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
+﻿using System;
 using System.Threading;
 using EasyModbus; // Packet for Modbus connection - Import EasyModbusCore
 using MQTTnet; //Packet for MQTT connection
 using MQTTnet.Client;
 using System.Text.Json;
+using System.Text;
 class Program
 {
+    //Var to store the newscale from Mqtt broker and share between task
+    static int iMqttNewScalePayload = 0;
     static void Main()
     {
         Console.WriteLine("Hello, Panasonic-Modbus!");
@@ -17,8 +17,8 @@ class Program
 
         // Creation of the client Modbus TCP
         ModbusClient modbusClient = new ModbusClient(ipAddress, port);
-
-        //.WithTcpServer("localhost", 1884)
+		
+		//.WithTcpServer("localhost", 1884) //tested ok window Docker 
         // setting MQTT
         var mqttFactory = new MqttFactory();
         var mqttClient = mqttFactory.CreateMqttClient();
@@ -29,32 +29,78 @@ class Program
         
         try
         {
+            /////////////////////////////////////////////////////////////////////////////////////////
             // Connect to Modbus
             Console.WriteLine($"Connecting a {ipAddress}:{port}");
             modbusClient.Connect(); // Connecting to Modbus Server
             Console.WriteLine("Modbus Connection established");
+            /////////////////////////////////////////////////////////////////////////////////////////
+
+            /////////////////////////////////////////////////////////////////////////////////////////
+            // MQTT handlers
+
+            // Register handlers before connecting
+            mqttClient.ConnectedAsync += async e =>
+            {
+                Console.WriteLine("MQTT Connection established");
+                // plant01/plc01/modbus/sensor01/temperature
+                await mqttClient.SubscribeAsync("plant01/plc01/modbus/sensor01/newscale"); //plant01/#
+            };
+
+            //Handler informs if there was a discconnection from broker
+            mqttClient.DisconnectedAsync += e =>
+            {
+                Console.WriteLine("Connection closed.");
+                return Task.CompletedTask;
+            };
+
+            //Handler to get the Mqtt paylodad from the subscrption, topic:  plant01/plc01/modbus/sensor01/newscale
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                string topic = e.ApplicationMessage.Topic;
+                string payload = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment);
+
+                Console.WriteLine($"Message received with the topic: {topic}");
+                Console.WriteLine($"Payload (JSON): {payload}\n");
+
+                iMqttNewScalePayload = int.Parse(payload);
+
+                int startAddressHR = 0;  // Address of the Holding register which will be written                    
+                modbusClient.WriteSingleRegister(startAddressHR, iMqttNewScalePayload); // Modbus Dir 40001 = index 0.  Writting 1 register
+                Console.WriteLine($"Writting Modbus Holding Register. Date Time: {DateTime.Now} - Value of Register {startAddressHR}: {iMqttNewScalePayload}");
+
+                //Check it using: mosquitto_pub -h localhost -p 1884 -t plant01/plc01/modbus/sensor01/newscale -m "2"
+                return Task.CompletedTask; 
+            }; 
 
             // Connect to MQTT
             Console.WriteLine("Connecting to broker MQTT...");
             mqttClient.ConnectAsync(mqttOptions).Wait();
-            Console.WriteLine("MQTT Connection established");
+            //Console.WriteLine("MQTT Connection established");
+            /////////////////////////////////////////////////////////////////////////////////////////
 
             while(true)
             {
                 try
                 {
-                    int startAddress = 0;  // Address of the Input register which will be read
-                    int[] registers = modbusClient.ReadInputRegisters(startAddress, 1); // Reading 1 register
-                    int registerValue = registers[0];
+                    //MODBUS reading fromm Input registers and writting to Holding registers
+                    int startAddressIR = 0;  // Address of the Input register which will be read
+                    int[] registers = modbusClient.ReadInputRegisters(startAddressIR, 2); // Modbus Dir 10001 = index 0.   Reading 2 register
+                    int registerTempValue = registers[0];
+                    int registerScaleValue = registers[1];
+                    Console.WriteLine($"Reading Modbus Input Register. Date Time: {DateTime.Now} - Value of Register {startAddressIR}: {registerTempValue} - Value of Register {startAddressIR+1}: {registerScaleValue}");
+                    
+                    //int startAddressHR = 0;  // Address of the Holding register which will be written                    
+                    //modbusClient.WriteSingleRegister(startAddressHR, iMqttNewScalePayload); // Modbus Dir 40001 = index 0.  Writting 1 register
 
-                    Console.WriteLine($"Date Time: {DateTime.Now} - Value of Register {startAddress}: {registerValue}");
-
-                    // Creating MQTT message with the register data 
+                    // Creating MQTT message with the register data to publish Temp value and scale
                     var data = new
                     {
                         sensor_id="sensorTemperature01",
-                        value = registerValue,
-                        unit = "C",
+                        temp_value = registerTempValue,
+                        temp_unit = "ºC",
+                        scale_value = registerScaleValue,
+                        scale_unit = "ºC/V",
                         timestamp = DateTime.Now.ToString("o")
                     };
                     string jsonPayload = JsonSerializer.Serialize(data);
@@ -66,8 +112,9 @@ class Program
                         .Build();
                     
                     mqttClient.PublishAsync(mqttMessage).Wait();
-                    Console.WriteLine($"Mensaje publicado: {jsonPayload}");
-
+                    Console.WriteLine($"Message published the topic: plant01/plc01/modbus/sensor01/temperature. Payload: {jsonPayload}");
+                    //check publising using: mosquitto_sub -h localhost -p 1884 -v -t plant01/plc01/modbus/sensor01/temperature
+                    //////////////////////////////////////////////////////////////////////////////////////////////
                 }
                 catch (Exception ex)
                 {
@@ -103,8 +150,6 @@ class Program
         }
     }
 }
-
-
 
 
 
