31c61196b7d4fa0e9693c71c5f8fc052119800cd
angresgarroalumnosupm.es <andresgarro@alumnos.upm.es.com>
2025-06-24 09:47:55 +0200

    2 seconds ppt

--- Program.cs
+++ Program.cs
@@ -122,7 +122,7 @@ class Program
                 }  
                 
                 // wait for 2 seconds before the next reading 
-                Thread.Sleep(3000);
+                Thread.Sleep(2000);
             }
         }
         catch (Exception ex)
